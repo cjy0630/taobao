174ddfbf6ca6f53cb8de937e30ef0d606400454a
cjy0630 <18500571066@163.com>
2019-07-05 09:03:46 +0800

    update

--- run.py
+++ run.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
             ipt = input('请输入要搜索的商品：')
             set_keyword(ipt)
             print('正在获取商品及评价，请耐心等待...')
-            # cmdline.execute('scrapy crawl alcohol'.split())
-            cmdline.execute('scrapy crawl alcohol -s LOG_FILE=all.log'.split())
+            cmdline.execute('scrapy crawl alcohol'.split())
+            # cmdline.execute('scrapy crawl alcohol -s LOG_FILE=all.log'.split())
         elif user_ipt == '2':
             get_now_settings()
         elif user_ipt == '3':
--- taobao/spiders/alcohol.py
+++ taobao/spiders/alcohol.py
@@ -16,22 +16,23 @@ class AlcoholSpider(scrapy.Spider):
     page_num = 1
     comment_flag = 0
 
+    offset = 0
+    count = 1
+    page = 0
+
     def start_requests(self):
         """
         重写start_requests()方法，在请求中加入headers
         :return: server response
         """
-        offset = 0
-        while self.flag == 0:
-            cookie = get_cookie()
-            headers = {
-                'cookie': cookie,
-                'referer': get_search_api() + get_keyword()
-            }
+        cookie = get_cookie()
+        headers = {
+            'cookie': cookie,
+            'referer': get_search_api() + get_keyword()
+        }
 
-            url = get_search_api() + get_keyword() + '&s=' + str(offset)
-            offset += 44
-            yield scrapy.Request(url, headers=headers, callback=self.parse)
+        url = get_search_api() + get_keyword() + '&s=0'
+        yield scrapy.Request(url, headers=headers, callback=self.parse)
 
     def parse(self, response):
         """
@@ -47,25 +48,33 @@ class AlcoholSpider(scrapy.Spider):
             data = response.css('script::text').extract()[3].split('\n')[2].strip()[16:].strip(';')
             data = json.loads(data)
             datas = data['mods']['itemlist']['data']['auctions']
+
             for data in datas:
                 item_id = data['nid']
                 user_id = data['user_id']
                 title = data['raw_title']
                 url = 'https:' + data['detail_url']
-                page = 1
-                while self.comment_flag == 0:
-                    cookie = get_cookie()
-                    headers = {
-                        'cookie': cookie,
-                        'referer': url
-                    }
-                    comment_url = get_comment_api() + item_id + '&sellerId=' + user_id + '&currentPage=' + str(page)
-                    page += 1
-                    yield scrapy.Request(comment_url, headers=headers, meta={'title': title, 'page': page - 1}, callback=self.parse_comment, dont_filter=True)
-                    time.sleep(random.randint(1, 5))
-            self.page_num += 1
-        except json.decoder.JSONDecodeError:
-            self.flag = 1
+                # print(item_id, user_id, title, url)
+                comment_url = get_comment_api() + item_id + '&sellerId=' + user_id + '&currentPage=1'
+                cookie = get_cookie()
+                headers = {
+                    'cookie': cookie,
+                    'referer': url
+                }
+                yield scrapy.Request(comment_url, headers=headers, meta={'referer': url, 'item_id': item_id, 'user_id': user_id}, callback=self.parse_comment, dont_filter=True)
+            self.offset += 44
+            cookie = get_cookie()
+            headers = {
+                'cookie': cookie,
+                'referer': get_search_api() + get_keyword()
+            }
+            search_url = get_search_api() + get_keyword() + '&s=' + str(self.offset)
+            yield scrapy.Request(search_url, headers=headers, callback=self.parse, dont_filter=True)
+            self.count += 1
+            time.sleep(random.randint(1, 5))
+        except Exception as e:
+            error = e
+            print('爬取结束共%d页' % (self.count - 1))
 
     def parse_comment(self, response):
         """
@@ -85,10 +94,24 @@ class AlcoholSpider(scrapy.Spider):
             for data in datas:
                 rate_content = data['rateContent']
                 rate_date = data['rateDate']
-                print('评价内容：%s  评价时间：%s' % (rate_content, rate_date))
+                print('商品ID：%s  商家ID：%s  评价内容：%s  评价时间：%s' % (response.meta['item_id'], response.meta['user_id'], rate_content, rate_date))
                 item['rate_content'] = rate_content
                 item['rate_date'] = rate_date
                 yield item
+            self.page += 1
+            cookie = get_cookie()
+            headers = {
+                'cookie': cookie,
+                'referer': response.meta['referer']
+            }
+            item_id = response.meta['item_id']
+            user_id = response.meta['user_id']
+            comment_url = get_comment_api() + item_id + '&sellerId=' + user_id + '&currentPage=' + str(self.page)
+            yield scrapy.Request(comment_url, headers=headers, callback=self.parse_comment, dont_filter=True)
+            time.sleep(random.randint(1, 5))
+        except Exception as e:
+            error = e
+            print('%s的所有评价爬取完成' % item_id)
         except json.decoder.JSONDecodeError:
             print('滑动验证出现了')
             print('解决方案：\n1、设置代理IP池，使用随机IP访问\n2、设置cookie池，使用随机cookie访问\n3、打开浏览器滑动一下重新复制并替换cookie')
