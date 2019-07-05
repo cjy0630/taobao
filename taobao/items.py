201e6e8a3c81ebef760d4b30535063869ad1fc48
cjy0630 <18500571066@163.com>
2019-07-05 10:39:30 +0800

    update

--- taobao/items.py
+++ taobao/items.py
@@ -18,10 +18,7 @@ class TaobaoItem(scrapy.Item):
     rate_date = scrapy.Field()
 
     def get_insert_sql(self):
-        # sql语句，用与向数据库中插入数据，此处我没有使用insert into，而是使用的replace into
-        # replace into是向数据库中插入数据时，根据主键进行判断，如果数据已经存在则先删除掉原有数据再插入新的数据，如果数据不存在则直接进行插入
-        # 由于考虑到用户可能修改评价，所以此处使用replace into进行插入，以保证所有数据都是最新的
-        insert_sql = 'replace into comment(keyword, item_id, user_id, title, rate_content, rate_date) values (%s, %s, %s, %s, %s, %s)'
+        insert_sql = 'insert into comment(keyword, item_id, user_id, title, rate_content, rate_date) values (%s, %s, %s, %s, %s, %s)'
         params = (
             self['keyword'],
             self['item_id'],
--- taobao/spiders/alcohol.py
+++ taobao/spiders/alcohol.py
@@ -15,6 +15,7 @@ class AlcoholSpider(scrapy.Spider):
     offset = 0
     count = 1
     page = 0
+    comment_count = 0
 
     def start_requests(self):
         """
@@ -50,14 +51,13 @@ class AlcoholSpider(scrapy.Spider):
                 user_id = data['user_id']
                 title = data['raw_title']
                 url = 'https:' + data['detail_url']
-                # print(item_id, user_id, title, url)
                 comment_url = get_comment_api() + item_id + '&sellerId=' + user_id + '&currentPage=1'
                 cookie = get_cookie()
                 headers = {
                     'cookie': cookie,
                     'referer': url
                 }
-                yield scrapy.Request(comment_url, headers=headers, meta={'referer': url, 'item_id': item_id, 'user_id': user_id}, callback=self.parse_comment, dont_filter=True)
+                yield scrapy.Request(comment_url, headers=headers, meta={'referer': url, 'item_id': item_id, 'user_id': user_id, 'title': title}, callback=self.parse_comment, dont_filter=True)
             self.offset += 44
             cookie = get_cookie()
             headers = {
@@ -70,7 +70,7 @@ class AlcoholSpider(scrapy.Spider):
             time.sleep(random.randint(1, 5))
         except Exception as e:
             error = e
-            print('爬取结束，共%d页商品' % (self.count - 1))
+            print('爬取结束，共%d页商品，%d条评价' % (self.count - 1, self.comment_count))
         except json.decoder.JSONDecodeError:
             print('滑动验证出现了')
             print('解决方案：\n1、设置代理IP池，使用随机IP访问\n2、设置cookie池，使用随机cookie访问\n3、打开浏览器滑动一下重新复制并替换cookie')
@@ -91,11 +91,19 @@ class AlcoholSpider(scrapy.Spider):
             data = json.loads(data)
             datas = data['rateDetail']['rateList']
             for data in datas:
+                item_id = response.meta['item_id']
+                user_id = response.meta['user_id']
+                title = response.meta['title']
                 rate_content = data['rateContent']
                 rate_date = data['rateDate']
-                print('商品ID：%s  商家ID：%s  评价内容：%s  评价时间：%s' % (response.meta['item_id'], response.meta['user_id'], rate_content, rate_date))
+                print('商品ID：%s  商家ID：%s  评价内容：%s  评价时间：%s' % (item_id, user_id, rate_content, rate_date))
+                item['keyword'] = get_keyword()
+                item['item_id'] = item_id
+                item['user_id'] = user_id
+                item['title'] = title
                 item['rate_content'] = rate_content
                 item['rate_date'] = rate_date
+                self.comment_count += 1
                 yield item
             self.page += 1
             cookie = get_cookie()
@@ -110,7 +118,6 @@ class AlcoholSpider(scrapy.Spider):
             time.sleep(random.randint(1, 5))
         except Exception as e:
             error = e
-            print('%s的所有评价爬取完成' % response.meta['item_id'])
         except json.decoder.JSONDecodeError:
             print('滑动验证出现了')
             print('解决方案：\n1、设置代理IP池，使用随机IP访问\n2、设置cookie池，使用随机cookie访问\n3、打开浏览器滑动一下重新复制并替换cookie')
