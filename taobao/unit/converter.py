0f2dd978e053fa91171ddf649da817c1d40fc401
cjy0630 <18500571066@163.com>
2019-07-07 14:26:19 +0800

    update

--- taobao/unit/converter.py
+++ taobao/unit/converter.py
@@ -3,6 +3,11 @@ import hashlib
 
 
 def to_md5(value):
+    """
+    用于将字符串转换为md5
+    :param value: 接收的待转字符串
+    :return: 转换为md5的字符串
+    """
     md5 = hashlib.md5()
     md5.update(value.encode(encoding='utf-8'))
     new_value = md5.hexdigest()
--- taobao/unit/removal.py
+++ taobao/unit/removal.py
@@ -7,6 +7,11 @@ from taobao.config.config_operate import get_db_settings
 
 
 def removal_grab_records(value):
+    """
+    用于评价页面url去重，根据评价页面url进行去重
+    :param value: 接收待比对的评价页面url
+    :return: 待抓取的评价页面url是否已存在（0是不存在，1是已存在）
+    """
     db_settings = get_db_settings()
     conn = pymysql.connect(host=db_settings[0], port=db_settings[1], user=db_settings[2], passwd=db_settings[3],
                            db=db_settings[4], charset='utf8')
@@ -24,6 +29,11 @@ def removal_grab_records(value):
 
 
 def removal_comment(value):
+    """
+    用于写入数据库时的评价去重，根据评价信息进行去重
+    :param value: 接收待比对的评价
+    :return: 待入库的评价信息是否已存在（0是不存在，1是已存在）
+    """
     db_settings = get_db_settings()
     conn = pymysql.connect(host=db_settings[0], port=db_settings[1], user=db_settings[2], passwd=db_settings[3],
                            db=db_settings[4], charset='utf8')
